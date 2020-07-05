c5a0d401e7131375d07767c8d4d97634c027ed2f
Samuel-dot-cloud <successam336@gmail.com>
2020-07-05 17:08:15 +0300

    Creating functions to direct to each layout

--- app/src/main/java/com/studiofive/soundgame/MainActivity.java
+++ app/src/main/java/com/studiofive/soundgame/MainActivity.java
@@ -8,6 +8,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        showMainScreen();
+    }
+
+    //Show main page
+    private void showMainScreen(){
         setContentView(R.layout.activity_main);
+    }
+
+    private void showDrinksPage(){
+        setContentView(R.layout.drinks);
+    }
+
+    private void showFoodsPage(){
+        setContentView(R.layout.foods);
+    }
+
+    private void showFruitsPage(){
+        setContentView(R.layout.fruits);
     }
 }
